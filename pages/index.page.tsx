938119286cafdfbd1ad5a71f36eec0659ed2b240
Josh Wootonn <joshuawootonn@gmail.com>
2022-08-08 07:46:27 -0500

    feat: update readme to be accurate

--- pages/index.page.tsx
+++ pages/index.page.tsx
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="w-full flex flex-col space-y-12 items-center justify-center">
       <h1 className="text-3xl">
-        Check out the different examples from (TODO: INSERT WEB LINK HERE)
+        The links below are live demos from: https://www.joshuawootonn.com/vercel-tabs-component 
       </h1>
       <Link href={"/css"}>CSS</Link>
       <Link href={"/transition-group"}>React Transition Group</Link>
